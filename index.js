1665549a506ef3e6a429dcb60f15f0e91ea34d9f
Thomas Maneschijn <thomasmaneschijn@gmail.com>
2021-10-21 21:39:54 +0200

    Refactor and add https support

--- index.js
+++ index.js
@@ -5,12 +5,38 @@
 
 // Dependencies
 const http = require('http');
+const https = require('https');
 const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
 const config = require('./config');
+const fs = require('fs');
 
-// The server should respond to all requests with a string
-const server = http.createServer((req, res) => {
+// Instantiate HTTP server
+const httpServer = http.createServer((req, res) => {
+    unifiedServer(req, res);    
+});
+
+// Start the HTTP server
+httpServer.listen(config.httpPort, () => {
+    console.log(`The HTTP server is listening on port ${config.httpPort}`);
+});
+
+// Instantiate HTTPS server
+const httpsServerOptions = {
+    'key': fs.readFileSync('./https/key.pem'),
+    'cert': fs.readFileSync('./https/cert.pem'),
+};
+const httpsServer = https.createServer(httpsServerOptions, (req, res) => {
+    unifiedServer(req, res);    
+});
+
+// Start the HTTPS server
+httpsServer.listen(config.httpsPort, () => {
+    console.log(`The HTTPS server is listening on port ${config.httpsPort}`);
+});
+
+// All the server logic for both HTTP and HTTPS
+const unifiedServer = (req, res) => {
 
     // Get the URL and parse it
     const parsedUrl = url.parse(req.url, true);
@@ -74,12 +100,7 @@ const server = http.createServer((req, res) => {
             // console.log('Request received with this payload: ', buffer);
         });
     });
-});
-
-// Start the server and have it listen to port 3000
-server.listen(config.port, () => {
-    console.log(`The server is listening on port ${config.port} in ${config.envName} mode`);
-});
+};
 
 // Define the handlers
 const handlers = {};
